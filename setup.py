ab27f20ab05ad26588efe99e0226b69c97b7b9c0
badebondt <47482159+badebondt@users.noreply.github.com>
2019-04-07 19:37:16 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 31 12:46:08 2019
+
+@author: bavodebondt
+"""
+
+from setuptools import setup
+
+setup(name='gym_crypto',
+      version='0.0.1',
+      install_requires=['gym']#And any other dependencies required
+)
